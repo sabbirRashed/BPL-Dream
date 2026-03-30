e157e151fb02e87a4032e6ffc8e4d165823fa141
sabbirRashed <srsabbirrahman12>
2026-03-30 11:23:23 +0600

    added banner bg with better ui

--- src/Components/HomePage/Banner/Banner.jsx
+++ src/Components/HomePage/Banner/Banner.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import heroLogo from '../../../assets/banner-main.png';
+import bannerImage from '../../../assets/banner-_syl.png';
 
 const Banner = () => {
     return (
         <div>
-            <div className="hero bg-[#79AE6F] min-h-[60vh] rounded-2xl mt-8 bg-linear-to-rt">
-                <div className="hero-content text-center">
+            <div className="hero min-h-[60vh] rounded-2xl mt-8 bg-cover bg-center relative bg-blend-overlay"
+                style={{backgroundImage:`url(${bannerImage})`}}>
+                <div className="hero-content text-center z-10">
                     <div className="space-y-6">
                         <img src={heroLogo} alt="" className='mx-auto' />
                         <h1 className="text-4xl text-white font-bold">Assemble Your Ultimate Dream 11 Cricket Team</h1>
                         <p className="text-2xl text-white">
                             Beyond Boundaries Beyond Limits
                         </p>
-                        <div className='p-1 rounded-xl  border-2 border-[#346739] inline-block'>
-                            <button className="btn btn-primary rounded-lg bg-[#346739] border-none shadow-none">Claim Free Credit</button>
+                        <div className='p-1 rounded-xl  border-2 border-white inline-block'>
+                            <button className="btn btn-primary rounded-lg bg-white text-[#346739] border-none shadow-none  hover:bg-[#79AE6F] hover:text-white">Claim Free Credit</button>
                         </div>
                     </div>
                 </div>
+                <div className='absolute inset-0 bg-[#285A48] rounded-2xl opacity-95 z-0'></div>
             </div>
+            
         </div>
     );
 };
